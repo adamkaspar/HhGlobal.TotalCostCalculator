2fd38d11a525d3956a99b5fd4f9d79ddf24d5e5c
Adam Kašpar <adam.kaspar@gmail.com>
2024-03-15 22:09:33 +0100

    TotalCostCalculator.Tests - added basic set of unit tests

---
+++ HhGlobal.TotalCostCalculator.Tests/UnitTests/JobCostCalculatorUnitTest.cs
@@ -0,0 +1,201 @@
+using Xunit;
+using Moq;
+using FluentAssertions;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using HhGlobal.TotalCostCalculator.BLL.Calculators;
+using HhGlobal.TotalCostCalculator.BLL.Common;
+using HhGlobal.TotalCostCalculator.BLL.Models;
+
+namespace HhGlobal.TotalCostCalculator.Tests.UnitTests;
+
+public class JobCostCalculatorUnitTest{
+    
+    Mock<IOptions<Configuration>> ConfigurationMock{ get; }
+
+    Mock<ILogger<JobCostCalculator>> LoggerMock{ get; }
+
+    IJobCostCalculator JobCostCalculator { get; }
+
+    public JobCostCalculatorUnitTest()
+    {
+        //Configuration setup
+        ConfigurationMock = new Mock<IOptions<Configuration>>();
+
+        ConfigurationMock
+            .Setup(configuration => configuration.Value)
+            .Returns(new Configuration{
+                SalesTax = 0.07,
+                BaseMargin = 0.11,
+                ExtraMargin = 0.05,
+                NumOfFractionalDigits = 2
+            });
+
+        //Logger setup
+        LoggerMock = new Mock<ILogger<JobCostCalculator>>();
+
+        //JobCostCalculator initialization
+        JobCostCalculator = new JobCostCalculator(ConfigurationMock.Object, LoggerMock.Object);
+    }
+
+    [Fact]
+    public async Task CalculateJobCostAsync_ShouldReturnJob_WithExtraMargin_ForItem_WithExtempt()
+    {                       
+        var Job = new Job{
+            IsExtraMargin = true,
+            PrintItems = new List<PrintItem>{
+                new PrintItem{
+                    Name = "yoyo", 
+                    Cost = 100,
+                    IsExempt = true
+                }
+            }
+        };
+
+        var result = await JobCostCalculator.CalculateJobCostAsync(Job, CancellationToken.None);
+        
+        result.Should().BeEquivalentTo(new JobResult{
+            PrintItems = new List<PrintItem>{
+                new PrintItem{
+                    Name = "yoyo", 
+                    Cost = 100,
+                    IsExempt = true
+                }                
+            },
+            Total = 116
+        });
+    }
+
+    [Fact]
+    public async Task CalculateJobCostAsync_ShouldReturnJob_WithExtraMargin_ForItem_WithoutExtempt()
+    {                       
+        var Job = new Job{
+            IsExtraMargin = true,
+            PrintItems = new List<PrintItem>{
+                new PrintItem{
+                    Name = "yoyo", 
+                    Cost = 100,
+                    IsExempt = false
+                }
+            }
+        };
+
+        var result = await JobCostCalculator.CalculateJobCostAsync(Job, CancellationToken.None);
+        
+        result.Should().BeEquivalentTo(new JobResult{
+            PrintItems = new List<PrintItem>{
+                new PrintItem{
+                    Name = "yoyo", 
+                    Cost = 107,
+                    IsExempt = false
+                }                
+            },
+            Total = 123
+        });
+    }
+
+    [Fact]
+    public async Task CalculateJobCostAsync_ShouldReturnJob_WithoutExtraMargin_ForItem_WithoutExtempt()
+    {                       
+        var Job = new Job{
+            IsExtraMargin = false,
+            PrintItems = new List<PrintItem>{
+                new PrintItem{
+                    Name = "yoyo", 
+                    Cost = 100,
+                    IsExempt = false
+                }
+            }
+        };
+
+        var result = await JobCostCalculator.CalculateJobCostAsync(Job, CancellationToken.None);
+        
+        result.Should().BeEquivalentTo(new JobResult{
+            PrintItems = new List<PrintItem>{
+                new PrintItem{
+                    Name = "yoyo", 
+                    Cost = 107,
+                    IsExempt = false
+                }                
+            },
+            Total = 118
+        });
+    }
+
+    [Fact]
+    public async Task CalculateJobCostAsync_ShouldReturnJob_WithExtraMargin_ForItem_WithoutExtempt_AndItem_WithExtempt()
+    {                       
+        var Job = new Job{
+            IsExtraMargin = true,
+            PrintItems = new List<PrintItem>{
+                new PrintItem{
+                    Name = "envelopes", 
+                    Cost = 520,
+                    IsExempt = false
+                },
+                new PrintItem{
+                    Name = "letterhead", 
+                    Cost = 1983.37,
+                    IsExempt = true
+                }
+            }
+        };
+
+        var result = await JobCostCalculator.CalculateJobCostAsync(Job, CancellationToken.None);
+        
+        result.Should().BeEquivalentTo(new JobResult{
+            PrintItems = new List<PrintItem>{
+                new PrintItem{
+                    Name = "envelopes", 
+                    Cost = 556.4,
+                    IsExempt = false
+                },
+                new PrintItem{
+                    Name = "letterhead", 
+                    Cost = 1983.37,
+                    IsExempt = true
+                }                
+            },
+            Total = 2940.3
+        });
+    }
+
+    [Fact]
+    public async Task CalculateJobCostAsync_ShouldReturnJob_WithExtraMargin_ForItem_WithExtempt_AndItem_WithExtempt()
+    {                       
+        var Job = new Job{
+            IsExtraMargin = true,
+            PrintItems = new List<PrintItem>{
+                new PrintItem{
+                    Name = "frisbees", 
+                    Cost = 19385.38,
+                    IsExempt = true
+                },
+                new PrintItem{
+                    Name = "yoyo", 
+                    Cost = 1829,
+                    IsExempt = true
+                }
+            }
+        };
+
+        var result = await JobCostCalculator.CalculateJobCostAsync(Job, CancellationToken.None);
+        
+        result.Should().BeEquivalentTo(new JobResult{
+            PrintItems = new List<PrintItem>{
+                new PrintItem{
+                    Name = "frisbees", 
+                    Cost = 19385.38,
+                    IsExempt = true
+                },
+                new PrintItem{
+                    Name = "yoyo", 
+                    Cost = 1829,
+                    IsExempt = true
+                }                
+            },
+            Total = 24608.68
+        });
+    }
+}
